3054f5fa05ef35d27537f807673ef76b95461517
Ariya Phengphon <ariya.phe@student.mahidol.edu>
2023-04-14 21:36:36 +0700

    all function implemented (not clean)

---
+++ AddDataToDatabase.py
@@ -0,0 +1,63 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    "databaseURL" : "https://facelogin-5c710-default-rtdb.firebaseio.com/",
+})
+
+# Create Students folder
+ref = db.reference('Students')
+
+data = {
+    "6188488":
+    {
+        "name" : "Joe Biden",
+        "major" : "Arts",
+        "starting_year" : "2000",
+        "bloodType" : "AB",
+        "year" : 4,
+        "last_authenticated" : "2022-04-10 03:45:09"
+    },
+    "6288123":
+    {
+        "name" : "Donald Trump",
+        "major" : "Law",
+        "starting_year" : "1998",
+        "bloodType" : "O",
+        "year" : 2,
+        "last_authenticated" : "2022-01-15 07:14:55"
+    },
+     "6388030":
+    {
+        "name" : "Kulawut Makkamoltham",
+        "major" : "ICT",
+        "starting_year" : "2020",
+        "bloodType" : "A",
+        "year" : 3,
+        "last_authenticated" : "2022-02-22 12:00:01"
+    },
+    "6388040":
+    {
+        "name" : "Ariya Phengphon",
+        "major" : "ICT",
+        "starting_year" : "2020",
+        "bloodType" : "B",
+        "year" : 3,
+        "last_authenticated" : "2022-12-26 11:45:00"
+    },
+    "6388176":
+    {
+        "name" : "Prawit Wongsuwon",
+        "major" : "ICT",
+        "starting_year" : "2000",
+        "bloodType" : "O",
+        "year" : 10,
+        "last_authenticated" : "2022-12-26 11:45:00"
+    },
+}
+
+# Send data to the DB
+for key, value in data.items():
+    ref.child(key).set(value)
---
+++ EncodeGenerator.py
@@ -0,0 +1,56 @@
+import os
+import cv2
+import face_recognition
+import pickle
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import storage
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    "databaseURL" : "https://facelogin-5c710-default-rtdb.firebaseio.com/",
+    "storageBucket" : "facelogin-5c710.appspot.com",
+})
+
+# Encode Images one-by-one
+# Add into list one-by-one
+
+# Import the 'Student' Images into the list
+folderPath = 'Images'
+pathList = os.listdir(folderPath)
+imgList = []
+studentIds = []
+for path in pathList:
+    imgList.append(cv2.imread(os.path.join(folderPath, path)))
+    # print(os.path.splitext(path)[0])
+    studentIds.append(os.path.splitext(path)[0])
+    
+    # Add Images folder in the Storage
+    fileName = f'{folderPath}/{path}'
+    bucket = storage.bucket()
+    blob = bucket.blob(fileName)
+    blob.upload_from_filename(fileName)
+print(studentIds)
+
+# Encode Images
+def findEncoding(imagesList):
+    encodeList = []
+    for img in imagesList:
+        # Convert BGR (opencv) to RGB (face-recog)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+print("Encoding Started...")
+encodeListKnown = findEncoding(imgList)
+# Map Encoding lists with Ids
+encodeListKnownWithIds = [encodeListKnown, studentIds]
+print("Encoding Completed!")
+
+# Map Encoding lists with Ids into the file (write)
+file = open("EncodeFile.p", "wb")
+pickle.dump(encodeListKnownWithIds, file)
+file.close()
+print("File Saved =)")
